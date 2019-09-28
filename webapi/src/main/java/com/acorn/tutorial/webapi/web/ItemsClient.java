a435335e4ec43a0c486f132c296219d611907764
Ken Alexandersson <ken.alexandersson@gmail.com>
2019-09-28 09:08:31 +0200

    Added feign fallback example

--- webapi/src/main/java/com/acorn/tutorial/webapi/web/ItemsClient.java
+++ webapi/src/main/java/com/acorn/tutorial/webapi/web/ItemsClient.java
@@ -1,13 +1,16 @@
 package com.acorn.tutorial.webapi.web;
 
+import feign.hystrix.FallbackFactory;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.Collections;
 import java.util.List;
 
-@FeignClient(name = "items-service")
+@FeignClient(name = "items-service", fallbackFactory = ItemsClient.ItemsServiceFeignClientFallbackFactory.class)
 @RibbonClient(name = "items-service")
 public interface ItemsClient {
 
@@ -16,4 +19,23 @@ public interface ItemsClient {
 
     @GetMapping("/items/{id}")
     Item getItem(@PathVariable Long id);
+
+    @Component
+    class ItemsServiceFeignClientFallbackFactory implements FallbackFactory<ItemsClient> {
+
+        @Override
+        public ItemsClient create(Throwable throwable) {
+            return new ItemsClient() {
+                @Override
+                public List<Item> getItems() {
+                    return Collections.singletonList(new Item(0L, "ken", 0));
+                }
+
+                @Override
+                public Item getItem(Long id) {
+                    return new Item(0L, "ken", 0);
+                }
+            };
+        }
+    }
 }
