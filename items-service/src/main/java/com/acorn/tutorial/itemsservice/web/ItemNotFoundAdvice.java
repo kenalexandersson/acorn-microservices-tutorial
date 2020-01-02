3b53e2aac11a4cf36c2b5f270178f0ea0ab12b32
Ken Alexandersson <ken.alexandersson@jeppesen.com>
2020-01-02 12:52:31 +0100

    minor fix

--- items-service/src/main/java/com/acorn/tutorial/itemsservice/web/ItemNotFoundAdvice.java
+++ items-service/src/main/java/com/acorn/tutorial/itemsservice/web/ItemNotFoundAdvice.java
@@ -1,5 +1,8 @@
 package com.acorn.tutorial.itemsservice.web;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,10 +12,17 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ControllerAdvice
 public class ItemNotFoundAdvice {
 
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    public ItemNotFoundAdvice(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
     @ResponseBody
     @ExceptionHandler(ItemNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String itemNotFoundHandler(ItemNotFoundException ex) {
-        return ex.getMessage();
+    public String itemNotFoundHandler(ItemNotFoundException ex) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(ex.getMessage());
     }
 }
